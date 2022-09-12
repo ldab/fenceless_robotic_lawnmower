d0b3c0b9d871bb308be6ba1d0a445323a37c8833
ldab <l.bispo@live.com>
2022-09-12 12:19:18 +0200

    work

--- src/mower.cpp
+++ src/mower.cpp
@@ -9,11 +9,49 @@
 #include "wifi_helper.h"
 #include "wifi_remote_control.h"
 
+#define U_CFG_APP_GNSS_UART 1
+
+// Bring in the application settings
+#include "u_cfg_app_platform_specific.h"
+
 #include <ubxlib.h>
 
-static bool wifi_control_enabled    = false;
-static uint8_t rc_value_index       = 0;
-static RoverMode current_rover_mode = DRIVE_TURN_NORMAL;
+#define ZED_RX   2
+#define ZED_TX   46
+#define ZED_BAUD 38400
+
+static const uDeviceCfg_t gDeviceCfg = {
+    .version    = 0,
+    .deviceType = U_DEVICE_TYPE_GNSS,
+    .deviceCfg =
+        {
+            .cfgGnss = {.moduleType     = U_GNSS_MODULE_TYPE_M9,
+                        .pinEnablePower = U_CFG_APP_PIN_GNSS_ENABLE_POWER,
+                        .pinDataReady   = -1},
+        },
+    .transportType = U_DEVICE_TRANSPORT_TYPE_UART,
+    .transportCfg =
+        {
+            .cfgUart = {.uart     = U_CFG_APP_GNSS_UART,
+                        .baudRate = ZED_BAUD,
+                        .pinTxd   = ZED_TX,
+                        .pinRxd   = ZED_RX,
+                        .pinCts   = U_CFG_APP_PIN_GNSS_CTS,
+                        .pinRts   = U_CFG_APP_PIN_GNSS_RTS},
+        },
+};
+// NETWORK configuration for GNSS
+static const uNetworkCfgGnss_t gNetworkCfg = {.type = U_NETWORK_TYPE_GNSS,
+                                              .moduleType =
+                                                  U_GNSS_MODULE_TYPE_M9,
+                                              .devicePinPwr       = -1,
+                                              .devicePinDataReady = -1};
+
+static uDeviceHandle_t devHandle           = NULL;
+
+static bool wifi_control_enabled           = false;
+static uint8_t rc_value_index              = 0;
+static RoverMode current_rover_mode        = DRIVE_TURN_NORMAL;
 static uint16_t rc_values[RC_NUM_CHANNELS][RC_FILTER_SAMPLES];
 
 static uint16_t get_controller_channel_value(uint8_t channel)
@@ -87,7 +125,7 @@ static void handle_corrections_sub(void *handler_args, esp_event_base_t base,
 }
 
 static void handle_key_sub(void *handler_args, esp_event_base_t base,
-                                   int32_t event_id, void *event_data)
+                           int32_t event_id, void *event_data)
 {
   esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
   if ((esp_mqtt_event_id_t)event_id == MQTT_EVENT_DATA &&
@@ -96,11 +134,28 @@ static void handle_key_sub(void *handler_args, esp_event_base_t base,
   }
 }
 
+static char latLongToBits(int32_t thingX1e7, int32_t *pWhole,
+                          int32_t *pFraction)
+{
+  char prefix = '+';
+
+  // Deal with the sign
+  if (thingX1e7 < 0) {
+    thingX1e7 = -thingX1e7;
+    prefix    = '-';
+  }
+  *pWhole    = thingX1e7 / 10000000;
+  *pFraction = thingX1e7 % 10000000;
+
+  return prefix;
+}
+
 void setup()
 {
   set_led(RED, FAST);
 
   Serial.begin(115200);
+
   gpios_init();
 
   esp_log_level_set("*", ESP_LOG_INFO);
@@ -114,12 +169,54 @@ void setup()
   wifi_controller_init(NULL, NULL, WIFI_CONTROLLER_AP);
   wifi_controller_register_connection_callback(&handle_wifi_controller_status);
 
-  while (!wifi_sta_connected)
+  while (!wifi_sta_connected && millis() < 20000)
     vTaskDelay(100);
 
   pp_init(NULL, NULL, NULL);
   pp_subscribe("/pp/Lb/eu", &handle_corrections_sub);
   pp_subscribe("/pp/ubx/0236/Lb", &handle_corrections_sub);
+
+  uLocation_t location;
+  int32_t whole    = 0;
+  int32_t fraction = 0;
+  int32_t returnCode;
+
+  // Set an out of range value so that we can test it later
+  location.timeUtc = -1;
+
+  // Initialise the APIs we will need
+  uPortInit();
+  uDeviceInit();
+
+  // Open the device
+  returnCode = uDeviceOpen(&gDeviceCfg, &devHandle);
+  uPortLog("Opened device with return code %d.\n", returnCode);
+
+  // You may configure GNSS as required here
+  // here using any of the GNSS API calls.
+
+  // Bring up the GNSS network interface
+  uPortLog("Bringing up the network...\n");
+  if (uNetworkInterfaceUp(devHandle, U_NETWORK_TYPE_GNSS, &gNetworkCfg) == 0) {
+
+    // Get location
+    if (uLocationGet(devHandle, U_LOCATION_TYPE_GNSS, NULL, NULL, &location,
+                     NULL) == 0) {
+      uPortLog("I am here: https://maps.google.com/?q=%c%d.%07d/%c%d.%07d\n",
+               latLongToBits(location.latitudeX1e7, &whole, &fraction), whole,
+               fraction,
+               latLongToBits(location.longitudeX1e7, &whole, &fraction), whole,
+               fraction);
+    } else {
+      uPortLog("Unable to get a location fix!\n");
+    }
+
+    // When finished with the GNSS network layer
+    uPortLog("Taking down GNSS...\n");
+    uNetworkInterfaceDown(devHandle, U_NETWORK_TYPE_GNSS);
+  } else {
+    uPortLog("Unable to bring up GNSS!\n");
+  }
 }
 
 void loop()
