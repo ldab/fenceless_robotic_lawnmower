bf7e7c97a68d522455e0f02e36c7c93f20bb7e2d
ldab <l.bispo@live.com>
2022-09-16 19:00:06 +0200

    left is right, right is left

--- lib/gpio/gpio.h
+++ lib/gpio/gpio.h
@@ -16,10 +16,10 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 
 #define MOTOR_STANDBY         14
-#define MOTOR_R1              11
-#define MOTOR_R2              47
-#define MOTOR_L1              10
-#define MOTOR_L2              39
+#define MOTOR_R1              10
+#define MOTOR_R2              39
+#define MOTOR_L1              11
+#define MOTOR_L2              47
 
 typedef enum { RED, GREEN, YELLOW, BLUE, CYAN, WHITE, PURPLE } colour_t;
 typedef enum { SLOW = 2, MEDIUM = 4, FAST = 6 } rate_t;
